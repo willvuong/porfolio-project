a4f9e5d6059751f03a74086f1e36049999b1102d
unknown <willvuong@hotmail.com>
2022-10-24 11:49:14 -0400

    about projects responsive

--- src/components/About.js
+++ src/components/About.js
@@ -5,22 +5,22 @@ const About = () => {
   return (
     <section className="About content-section" id="about">
 
-      <div className="content-section-heading-container">
+      {/* <div className="content-section-heading-container">
         <h2 className="content-section-heading">about</h2>
-      </div>
-      
+      </div> */}
       
       <div className="about-container">
-        <div className="portfolio-img-container">
-          <img className="portfolio-img" src="portfolio-img.jpg" alt="WV" />
-        </div>
-
         <div className="portfolio-bio">
           <p>I am an open-minded, self-starter with a passion for all things Web Development.
             My passion for development stems from my desire to constantly learning about the 
             latest and greatest web technologies, problem solving, and a need to create. 
           </p>
-          <a id="portfolio-contact-button" href="mailto:willvuong@hotmail.com">contact</a>
+          {/* <a id="portfolio-contact-button" href="mailto:willvuong@hotmail.com">contact</a> */}
+        </div>
+
+        <div className="portfolio-img-container">
+          <img className="portfolio-img" src="portfolio-img.jpg" alt="WV" />
+          <div className="portfolio-img-background"></div>
         </div>
       </div>
       
