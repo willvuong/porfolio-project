9e47c3198ae4861ddcc7d452d3752fa7a98851ac
unknown <willvuong@hotmail.com>
2022-11-01 01:19:36 -0400

    full screen menu

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,14 +1,29 @@
 import React from 'react'
 
-import Navigation from './Navigation'
+import Menu from './Menu'
 
 const Header = () => {
+  const openMenu = () => {
+    document.querySelector(".Menu").classList.toggle('menu-open');
+    document.querySelector('body').classList.toggle('stop-scrolling');
+  }
+
   return (
     <header className="Header" id="header">
-      <div className="website-logo-container">
-        <a href="/"><img className="website-logo" src="website-logo.png" alt="WV" /></a>
-      </div>
-      <Navigation />
+      
+      <nav className="navigation">
+        <div className="website-logo-container">
+          <a href="/"><img className="website-logo" src="website-logo.png" alt="WV" /></a>
+        </div>
+
+        <div className="menu-button">
+          <input type="checkbox" className="toggle-menu" onClick={openMenu}></input>
+          <div className="hamburger"></div>
+        </div>
+      </nav>
+
+      <Menu />
+
     </header>
   )
 }
---
+++ src/components/layout/Menu.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Menu = () => {
+  const toggleMenu = () => {
+    document.querySelector(".toggle-menu").checked = false;
+  }
+
+  return (
+    <nav className="Menu">
+      <div className="menu-section">
+        <div className="menu-socials">
+          <a href="https://www.linkedin.com/in/willlvuong">LinkedIn</a>
+          <a href="https://github.com/willvuong">GitHub</a>
+        </div>
+
+        <div className="menu-contact">
+          <a href="tel:647-708-5527"><i class="fa-solid fa-phone"></i> +1 (647)-708-5527</a>
+          <a href="mailto:willvuong@hotmail.com"><i class="fa-solid fa-envelope"></i> willvuong@hotmail.com</a>
+        </div>
+      </div>
+
+      <ul className="menu-links">
+        <li><Link to="/resume" onClick={toggleMenu}>Resume</Link></li>
+        <a href="#contact" onClick={toggleMenu}><li id="contact-button">Contact</li></a>
+      </ul>
+    </nav>
+  )
+}
+
+export default Menu
